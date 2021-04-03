aa9b7fad3373418ce966b71eaa34739635c4c70b
Burak Duman <b.duman@amatis.nl>
2021-04-03 22:25:09 +0300

    Test

--- index.js
+++ index.js
@@ -2,7 +2,10 @@ var app = require('express')();
 var http = require('http').createServer(app);
 var io = require('socket.io')(http);
 
-
+app.get('/', function(req, res){
+    res.sendFile(__dirname + '/index.html');
+  });
+  
 io.on('connection', function(socket){
     console.log('a user connected');
     //Taking username
